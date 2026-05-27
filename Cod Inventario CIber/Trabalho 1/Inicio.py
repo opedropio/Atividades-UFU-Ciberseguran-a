85845e3de983462acb7957cebfcb04bc0b649294
Pedro Pio <ped.pio@ufu.br>
2026-05-27 11:04:27 -0300

    Menu e opçoes
    Criação do menu do inventario e validação das opçoes com funçoes

--- Cod Inventario CIber/Trabalho 1/Inicio.py
+++ Cod Inventario CIber/Trabalho 1/Inicio.py
@@ -32,7 +32,8 @@ while True:
 print(" REALIZE O LOGIN")
 
 while True:
-    usuario_login = print(f"Usuário:{nome} ")
+    print(f"Usuário:{nome} ")
+    usuario_login = nome
     senha_login = input("Senha: ")
 
     if senha_login == senha_criada:
@@ -51,12 +52,14 @@ ativos = {}
 
 def menu():
     print("\n ----- LISTA DE ATIVOS INVENTARIO CIBER -----")
-    print("1 - Inserir").lower()
-    print("2 - Listar").lower()
-    print("3 - Responsavel").lower()
-    print("4 - Remover").lower()
-    print("5 - Sair").lower()
+    print("1 - Inserir")
+    print("2 - Listar")
+    print("3 - Lista por ordem")
+    print("4 - Atualizar")
+    print("5 - Remover")
+    print("6 - Sair")
 
+#Cadastrar
 def cadastrar_ativos(ativos):
     try:
         id_ativos = int(input("ID do Ativo "))
@@ -74,13 +77,127 @@ def cadastrar_ativos(ativos):
             "Vulnerabilidade": vulnerabilidade
         }
 
-        print(f'{ativo} cadastrado com sucesso!')
+        print(f'{objeto} cadastrado com sucesso!')
 
     except ValueError:
         print("Ativo inválido.")
 
-cadastrar_ativos(ativos)
+#cadastrar_ativos(ativos)
+
+#Listar    
+def listar_ativos(ativos):
+    if not ativos: 
+        print("Nenhum ativo cadastrado.")
+        return
     
+    for id_ativos, dados in ativos.items():
+        print("\n-----------------")
+        print(f'ID: {id_ativos}')
+        print(f'Nome: {dados["Ativo"]}')
+        print(f'Responsável: {dados["Responsável"]}')
+        print(f'Vulnerabilidade: {dados["Vulnerabilidade"]}')
+
+
+#Listar ordenadamente
+def listar_ativos_ordenados(ativos):
+    if not ativos: 
+        print("Nenhum ativo cadastrado.")
+        return
+    ordenados = sorted(ativos.items(), key= lambda item: item [1] ["Ativo"])
+
+    print ('\n ---- Ativos ordenados por nome ------')
+    for id_ativos, dados in ordenados: 
+        print(f'{dados["Ativo"]} ({id_ativos})')
+
+#listar_ativos(ativos)
+#listar_ativos_ordenados(ativos)
+
+#Atulizar inventario 
+
+def atualizar_ativos(ativos):
+    try: 
+        id_ativos = int(input("ID do ativo a atulizar"))
+
+        if id_ativos not in ativos:
+            print("Ativo não encontrado")
+            return
+        print("Deixe em branco para manter o valor atual")
+
+        nome = input (f'Nome ({ativos[id_ativos] ["Ativo"]}):')
+        responsavel = input (f'Responsável ({ativos[id_ativos] ["Responsável"]}):')
+        vulnerabilidade = input (f'Vulnerabilidade ({ativos[id_ativos] ["Vulnerabilidade"]}):')
+
+        if nome: 
+            ativos[id_ativos]["Ativo"] = nome
+        if responsavel:
+            ativos[id_ativos]["Responsável"] = responsavel
+        if vulnerabilidade:
+            ativos[id_ativos]["Vulnerabilidade"] = vulnerabilidade
+
+        print ("Ativo atulizado com sucesso!")
+
+    except ValueError:
+        print("Dados inderidos inválidos.")
+
+#atualizar_ativos(ativos)
+#listar_ativos(ativos)
+
+
+#DELETE
+
+def apagar_ativo(ativos):
+    try:
+        id_ativos = int (input('ID do ativo para remover: '))
+
+        if ativos.pop(id_ativos, None):
+            print("Ativo removido com sucesso.")
+        else:
+            print("Ativo não encontrado.")
+
+    except ValueError:
+        print("Id inválido")
+
+#apagar_ativo(ativos)
+#listar_ativos(ativos)
+
+while True:
+    menu()
+    opcao = input ("Escolha uma opção: ")
+
+    match opcao: 
+        case '1': 
+            cadastrar_ativos(ativos) 
+        case '2': 
+            listar_ativos(ativos) 
+        case '3': 
+            listar_ativos_ordenados(ativos) 
+        case '4':
+            atualizar_ativos(ativos) 
+        case '5':
+            apagar_ativo(ativos) 
+        case '6': 
+            print("Saindo...")
+            break
+        case _:
+            print("Digite uma opção válida!")
+
+
+             
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
 
 
 
